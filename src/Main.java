57ae18e8b615c0464bdd67b258e2143caa0d8ed3
pingithefrosty <barni.kadar@gmail.com>
2018-02-13 16:17:20 +0100

    fixed the fact Sophie cannot use the styleguide

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,11 @@ public class Main {
 
   public void count() {
     int age = 26;
-    System.out.println("I am "+ age +" years old.");
-
-    public void cheer() {
-      String teamName = "Epidote";
-      System.out.println(teamName);
-    }
+    System.out.println("I am " + age + " years old.");
+  }
 
   public void future() {
     System.out.println("Taurus");
-  }
-
-  public void Count() {
-    System.out.println("I am 26 years old.");
   }
 
   public void cheer() {
