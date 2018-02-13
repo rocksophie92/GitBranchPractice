d35e718346990a5720641dbc41e0e15ff33098d7
nguyenannie <tramtramrhinomo@gmail.com>
2018-02-13 16:14:31 +0100

    a1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-<<<<<<< HEAD
+
   public void count() {
     int age = 26;
     System.out.println("I am "+ age +" years old.");
@@ -8,22 +8,21 @@ public class Main {
       String teamName = "Epidote";
       System.out.println(teamName);
     }
-=======
-<<<<<<< HEAD
 
   public void future() {
     System.out.println("Taurus");
-=======
-<<<<<<< HEAD
-  public void Count(){
-    System.out.println("I am 26 years old.");
-=======
+  }
 
-  public void cheer () {
+  public void Count() {
+    System.out.println("I am 26 years old.");
+  }
+
+  public void cheer() {
     String teamName = "Epidote";
     System.out.println(teamName);
->>>>>>> 0f575cc0a11958dd129ed50c521e8f179c6f737a
->>>>>>> 26ba3ec3d55bb8e67e2b2b75c72585f6efd2adac
->>>>>>> 3ab64ee943cc84cfe0dfab17f518069c853c3dce
   }
+
+  public void greet() {
+    System.out.println("Annie");
+    }
 }
