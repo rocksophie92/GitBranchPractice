83d0deb4f8915bd57a64a21fe614fbaad6ad8713
pingithefrosty <barni.kadar@gmail.com>
2018-02-13 15:49:54 +0100

    b2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
-<<<<<<< HEAD
-  public void Count(){
-    System.out.println("I am 26 years old.");
-=======
+  public void count() {
+    int age = 26;
+    System.out.println("I am "+ age +" years old.");
 
-  public void cheer () {
-    String teamName = "Epidote";
-    System.out.println(teamName);
->>>>>>> 0f575cc0a11958dd129ed50c521e8f179c6f737a
+    public void cheer() {
+      String teamName = "Epidote";
+      System.out.println(teamName);
+    }
   }
 }
